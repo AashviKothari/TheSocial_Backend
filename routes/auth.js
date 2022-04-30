dea5734c4f32d387472c0d401cb6779f49130d03
Aashvi Kothari <aashvikothari78@gmail.com>
2022-04-30 17:01:46 +0530

    Second Commit

--- routes/auth.js
+++ routes/auth.js
@@ -51,7 +51,7 @@ router.post("/login", async (req,res)=>{
         const validPassword = await bcrypt.compare(req.body.password, user.password)
         !validPassword && res.status(400).json("wrong password")
 
-        res.status(200).json(user)
+        validPassword &&   res.status(200).json(user)
     } catch(err) {
         res.status(500).json(err)
     }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
-const User = require("../models/User.js")
+const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
 
 //Update User
+router.put("/:id", async(req,res)=>{
+    if(req.body.userId == req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt)
+            } catch(err){
+                return res.status(500).json(err)
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("AUS")
+        }catch(err)
+        {
+            return res.status(500).json(err)
+        }
+    } else{
+        return res.status(403).json("You can only Update your Account")
+    }
+})
 //Delete User
 //get a user
 //get all user
