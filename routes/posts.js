b49a01a702e4cd41d18e33cdc4e0df5c7664b87d
Aashvi Kothari <aashvikothari78@gmail.com>
2022-05-02 00:18:49 +0530

    Second Commit

--- routes/posts.js
+++ routes/posts.js
@@ -87,6 +87,7 @@ router.get("/timeline", async(req,res)=>
                 return Post.find({userId:friendId});
             })
         );
+        
         res.json(userPosts.concat(...friendPosts))
     }catch(err){
         res.status(500).json(err)
