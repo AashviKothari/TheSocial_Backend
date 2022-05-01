dce5804cccbcdf078cba1662df6b722640730a01
Aashvi Kothari <aashvikothari78@gmail.com>
2022-05-01 10:25:46 +0530

    Third Commit

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 
 //Update User
 router.put("/:id", async(req,res)=>{
-    if(req.body.userId == req.params.id || req.user.isAdmin){
+    if(req.body.userId == req.params.id || req.body.isAdmin){
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10);
@@ -25,8 +25,32 @@ router.put("/:id", async(req,res)=>{
     } else{
         return res.status(403).json("You can only Update your Account")
     }
-})
+});
 //Delete User
+router.delete("/:id", async(req,res)=>{
+    if(req.body.userId == req.params.id || req.body.isAdmin){
+        
+        try{
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("ADS")
+        }catch(err)
+        {
+            return res.status(500).json(err)
+        }
+    } else{
+        return res.status(403).json("You can only Delete your Account")
+    }
+});
 //get a user
+
+router.get("/:id", async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {password, updatedAt, ...other} = user._doc
+        res.status(200).json(other)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 //get all user
 module.exports = router
