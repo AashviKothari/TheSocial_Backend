a524069e7fbd26d4689976a302ee23a9a0d2d00f
Aashvi Kothari <aashvikothari78@gmail.com>
2022-05-01 14:04:38 +0530

    Third Commit

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const Post = require("../models/Post")
+const Post = require("../models/Post");
+const User = require("../models/User");
 
 //create a post
 router.post("/",async(req,res)=>{
@@ -68,6 +69,25 @@ router.delete("/:id", async(req,res)=>{
         const post = await Post.findById(req.params.id);
         // const {password, updatedAt, ...other} = user._doc
         res.status(200).json(post)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//get timeline post
+
+router.get("/timeline", async(req,res)=>
+{
+    // let postArray = [];
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId: currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.followings.map(friendId=>{
+                return Post.find({userId:friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
     }catch(err){
         res.status(500).json(err)
     }
